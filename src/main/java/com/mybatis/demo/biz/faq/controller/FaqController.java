7f68d2e99dda2827a93e984cc7f3737a9d822763
rudxo <rudxo1588@naver.com>
2022-03-27 20:50:59 +0900

    file업로드 해보기, window Simbolic link 사용

--- src/main/java/com/mybatis/demo/biz/faq/controller/FaqController.java
+++ src/main/java/com/mybatis/demo/biz/faq/controller/FaqController.java
@@ -1,8 +1,11 @@
 package com.mybatis.demo.biz.faq.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
@@ -18,7 +21,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.mybatis.demo.biz.faq.service.FaqImgService;
@@ -79,6 +84,15 @@ public class FaqController {
 		model.addAttribute("id", session.getAttribute("id"));
 		return "faq/faqWrite";
 	}
+	
+	/**
+	 * file 등록페이지로 이동
+	 * @return
+	 */
+	@GetMapping("/addByFile")
+	public String file() {
+		return "faq/file";
+	}
 
 	/**
 	 * faq 등록하기(@Valid 체크해서 유효성검사)
@@ -87,6 +101,41 @@ public class FaqController {
 	@PostMapping("/add")
 	public void add(@RequestBody @Valid Faq faq) {
 		faqService.add(faq);
+	}
+	
+	
+	// required = true면 해당 requestParam의 value값이 없다면 error
+	@PostMapping("/addByMultiPart")
+	@ResponseBody
+	public String addByMultiPart(@RequestParam(value = "file" , required = false) MultipartFile file, Faq faq) throws Exception {
+		System.out.println(file.getOriginalFilename());
+		System.out.println(faq.getFaqTitle());
+		
+		if(!file.isEmpty()) {
+			String fileNm = file.getOriginalFilename().replaceAll(" ", "");
+			String regex = "^([\\S]+(\\.(?i)(jpg|jpeg|png|gif|bmp))$)";
+			
+			if(!fileNm.matches(regex)) {
+				throw new Exception("이미지만 등록해주세요");
+			}
+			
+			int idx = fileNm.lastIndexOf(".");
+			String extNm = fileNm.substring(idx);
+			
+			UUID uuid = UUID.randomUUID();	// 파일명이 겹치지 않도록 난수생성하여 파일명 만들어준다.
+			String saveFIleNm = uuid + extNm;	
+			
+			
+			File f = new File("C://upload");
+			if(!f.exists()) f.mkdirs(); // 폴더가 없다면 폴더 생성
+			
+			// File.separator / , \ 둘중 하나를 생성
+			File filepath = new File("c://upload" + File.separator + saveFIleNm);
+			
+			file.transferTo(filepath);
+		}
+		
+		return "success";
 	}
 	
 	/**
